bdb2d4c8fea410549ec08dbeb4b1029f546b8746
Tomato6966 <chris.pre03@gmail.com>
2022-10-22 20:53:45 +0200

    Update BotClient.mjs

--- src/structures/BotClient.mjs
+++ src/structures/BotClient.mjs
@@ -191,7 +191,7 @@ export class BotClient extends Client {
                                 this.buildOptions(command, Slash)
                                 return Slash;
                             });
-                            command.commandId = cache?.fetchedApplication?.find?.(c => c?.name == subSlash.name)?.permissions?.commandId ?? "commandId";
+                            command.commandId = this.botCache?.fetchedApplication?.find?.(c => c?.name == subSlash.name)?.permissions?.commandId ?? "commandId";
                             command.slashCommandKey = `/${subSlash.name} ${command.name}`
                             command.mention = `<${command.slashCommandKey}:${command.commandId}>`
                             // FOLDERSTRUCTURE: /commands/slash/XYZ/cmd.js
